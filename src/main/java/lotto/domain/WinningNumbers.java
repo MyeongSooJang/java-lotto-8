a98bebf4eee3d06661f5461324d0e785a2d90fdc
MyeongSooJang <mmsgod0427@naver.com>
2025-11-01 14:37:00 +0900

    feat: WinningNumbers 도메인 구현

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
+public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+
+    public WinningNumbers(List<Integer> winningNumbers) {
+        validateCount(winningNumbers);
+        validateRange(winningNumbers);
+        this.winningNumbers = winningNumbers;
+    }
+
+    private void validateCount(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> winningNumbers) {
+        for (Integer winningNumber : winningNumbers) {
+            if (1 > winningNumber || winningNumber > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public int countMatches(Lotto lotto) {
+        return lotto.countMatches(winningNumbers);
+    }
+
+}
