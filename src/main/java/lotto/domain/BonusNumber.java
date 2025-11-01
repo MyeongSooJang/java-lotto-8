826b7b07cb795380c75cc615b86fa2de6c63938b
MyeongSooJang <mmsgod0427@naver.com>
2025-11-01 15:00:32 +0900

    feat: BonusNumber 당첨번호 중복 메소드 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -3,13 +3,20 @@ package lotto.domain;
 public class BonusNumber {
     private final int bonusNumber;
 
-    public BonusNumber(int input) {
+    public BonusNumber(WinningNumbers winningNumbers, int input) {
         validateRange(input);
+        validateDuplicate(winningNumbers, input);
         this.bonusNumber = input;
     }
 
     private void validateRange(int input) {
         if (input < 1 || input > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(WinningNumbers winningNumbers, int input) {
+        if (winningNumbers.containNumber(input)) {
             throw new IllegalArgumentException();
         }
     }
