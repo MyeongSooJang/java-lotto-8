b93166f0d8cf69c1e6653e55434674d57f09f75b
MyeongSooJang <mmsgod0427@naver.com>
2025-11-01 15:20:33 +0900

    test: PurchaseAmount 도메인 테스트 구현

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class PurchaseAmountTest {
+
+    @Test
+    void 입력한_금액으로_생성이_된다() {
+        int input = 10000;
+
+        assertThatCode(() -> new PurchaseAmount(input))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 음수를_입력하는_경우_예외를_발생시킨다() {
+        int input = -10000;
+
+        assertThatThrownBy(() -> new PurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 천원_단위가_아닌_값을_입력하는_경우_예외를_발생시킨다() {
+        int input = 10100;
+
+        assertThatThrownBy(() -> new PurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음수를_입력하는_경우_예외를_발생시킨다() {
+        int input = -10000;
+
+        assertThatThrownBy(() -> new PurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구매한_로또_개수를_반환한다() {
+        int input = 10000;
+
+        PurchaseAmount purchaseAmount = new PurchaseAmount(input);
+
+        assertThat(purchase.calculateLottoCount()).isEqualTo(10);
+    }
+}
