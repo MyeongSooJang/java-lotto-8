39b3f1952daf9a7cfcaa25b733304a0f43cf9389
MyeongSooJang <mmsgod0427@naver.com>
2025-11-02 15:10:00 +0900

    feat: LottoBundle 도메인 구현

---
+++ src/main/java/lotto/domain/LottoBundle.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoBundle {
+    private final List<Lotto> lottos;
+
+    public LottoBundle(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public int getSize() {
+        return lottos.size();
+    }
+
+    public LottoResult checkWinning(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        Map<Rank, Integer> rankCounts = createRankCounts();
+
+        for (Lotto lotto : lottos) {
+            Rank rank = lotto.match(winningNumbers, bonusNumber);
+            rankCounts.put(rank, rankCounts.get(rank) + 1);
+        }
+
+        return new LottoResult(rankCounts);
+    }
+
+    private Map<Rank, Integer> createRankCounts() {
+        Map<Rank, Integer> rankCounts = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            rankCounts.put(rank, 0);
+        }
+        return rankCounts;
+    }
+}
